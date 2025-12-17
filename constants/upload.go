294752432186451dd20f36091968e1b2f37eaba3
rizalarfani <arfanirizal22@gmail.com>
2025-12-17 08:49:47 +0700

    updated: webhooks URL

--- constants/upload.go
+++ constants/upload.go
@@ -2,5 +2,5 @@ package constants
 
 const (
 	UploadBrandUrl       = "https://hesda-store.com/upload/brand"
-	DigiflazzWebhooksUrl = "https://378950524ea2.ngrok-free.app/api/v1/transaction/webhooks"
+	DigiflazzWebhooksUrl = "https://api-ppob.hesda-store.com/api/v1/transaction/webhooks"
 )
